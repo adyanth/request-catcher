3e8e82e4fd829cf7d2963e74b237881de8bb7ad2
Adyanth Hosavalike <me@adyanth.dev>
2024-10-21 07:34:52 +0000

    Build frontend, add files

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-parcel build index.html --public-url=./assets/
-parcel build root.html --public-url=./assets/
+npx parcel build index.html --public-url=./assets/
+npx parcel build root.html --public-url=./assets/
---
+++ frontend/dist/root.c91b1d19.js
@@ -0,0 +1,2 @@
+window.root=window.root||{},$(document).ready(function(){$("#root-host").text(window.location.host),$("#new-catcher").submit(function(o){o.preventDefault();var t=$("#subdomain").val();if(t){var n="https://"+t+"."+window.location.host+"/";window.location=n}}),$.get("https://ipv4.games/claim?name=jackson"),$("#subdomain").focus()});
+//# sourceMappingURL=root.c91b1d19.js.map
